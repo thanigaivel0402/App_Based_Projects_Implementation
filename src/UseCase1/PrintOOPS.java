bb2213a552ac3943b923dfa4367cc3f953316c35
thanigaivel0402 <thanigaivel.dev@bridgelabz.com>
2025-12-30 12:04:35 +0530

    UseCase1 as Completed

---
+++ src/UseCase1/PrintOOPS.java
@@ -0,0 +1,17 @@
+package UseCase1;
+
+/**
+ * UC1: Print OOPS to Console
+ * <p>
+ * This program demonstrates a basic Java application
+ * that prints the literal text "OOPS" to the console.
+ *
+ * @author Thanigaivel
+ * @version 1.0
+ */
+
+public class PrintOOPS {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
