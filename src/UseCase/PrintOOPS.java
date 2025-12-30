b07edfa190c4aa4ecd910735a3a1e502bf8d5889
thanigaivel0402 <thanigaivel.dev@bridgelabz.com>
2025-12-30 14:20:29 +0530

    UseCase3 Completed

--- src/UseCase/PrintOOPS.java
+++ src/UseCase/PrintOOPS.java
@@ -1,39 +1,83 @@
 package UseCase;
 
 /**
- * UC2: Render OOPS as Banner
- *
- * This program prints the word "OOPS" in a banner format
- * using spaces and '*' characters.
- *
- * Height  : 7 lines
- * Width   : 9 characters per letter
+ * UC3: Refactor Banner Logic into Functions
+ * <p>
+ * This program renders the "OOPS" banner by refactoring
+ * banner-building logic into reusable static methods.
+ * <p>
+ * Output remains same as UC2.
  *
  * @author Thanigaivel
- * @version 2.0
+ * @version 3.0
  */
+class PrintOOPS {
 
-public class PrintOOPS {
     public static void main(String[] args) {
-        String[] banner = {
-                String.join("  ",
-                        " *** ", " *** ", "**** ", " ****"),
-                String.join("  ",
-                        "*   *", "*   *", "*   *", "*    "),
-                String.join("  ",
-                        "*   *", "*   *", "**** ", " ****"),
-                String.join("  ",
-                        "*   *", "*   *", "*    ", "    *"),
-                String.join("  ",
-                        "*   *", "*   *", "*    ", "    *"),
-                String.join("  ",
-                        "*   *", "*   *", "*    ", "*   *"),
-                String.join("  ",
-                        " *** ", " *** ", "*    ", " ****")
-        };
 
-        for (String line : banner) {
+        String[] banner = buildPattern();
+
+        printPattern(banner);
+    }
+
+    static String[] buildPattern() {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i];
+        }
+
+        return banner;
+    }
+
+
+    static void printPattern(String[] pattern) {
+        for (String line : pattern) {
             System.out.println(line);
         }
+    }
+
+    // Encapsulated pattern for letter O
+    static String[] getOPattern() {
+        return new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
+    }
+
+    // Encapsulated pattern for letter P
+    static String[] getPPattern() {
+        return new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    ",
+                "*    "
+        };
+    }
+
+    // Encapsulated pattern for letter S
+    static String[] getSPattern() {
+        return new String[]{
+                " ****",
+                "*    ",
+                " ****",
+                "    *",
+                "    *",
+                "*   *",
+                " ****"
+        };
     }
 }
